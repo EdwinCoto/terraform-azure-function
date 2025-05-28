9b23af28b2d7e3a6dec7cc93499b45c30bafcf1c
EdwinCoto <edcontreras@baufest.com>
2025-05-28 16:50:19 -0500

    fix: deprecated resources

--- main.tf
+++ main.tf
@@ -3,31 +3,39 @@ resource "azurerm_resource_group" "function_rg" {
   location = var.location
 }
 
-resource "azurerm_app_service_plan" "function_plan" {
-  name                = "${var.function_name}-plan"
-  location            = azurerm_resource_group.function_rg.location
-  resource_group_name = azurerm_resource_group.function_rg.name
-  sku {
-    tier     = "Dynamic"
-    size     = "Y1"
-  }
-}
-
-resource "azurerm_function_app" "function_app" {
-  name                       = var.function_name
-  location                   = azurerm_resource_group.function_rg.location
-  resource_group_name        = azurerm_resource_group.function_rg.name
-  app_service_plan_id        = azurerm_app_service_plan.function_plan.id
-  storage_account_name       = "${var.function_name}storage"
-  storage_account_access_key = azurerm_storage_account.function_storage.primary_access_key
-  version                    = "~3"
-  os_type                    = "linux"
-}
-
 resource "azurerm_storage_account" "function_storage" {
   name                     = "${var.function_name}storage"
   resource_group_name      = azurerm_resource_group.function_rg.name
   location                 = azurerm_resource_group.function_rg.location
-  account_tier            = "Standard"
+  account_tier             = "Standard"
   account_replication_type = "LRS"
+}
+
+resource "azurerm_service_plan" "function_plan" {
+  name                = "${var.function_name}-plan"
+  location            = azurerm_resource_group.function_rg.location
+  resource_group_name = azurerm_resource_group.function_rg.name
+  sku_name            = "Y1" # Dynamic SKU for Azure Functions
+  os_type             = "windows"
+
+}
+
+resource "azurerm_windows_function_app" "function_app" {
+  name                = var.function_name
+  location            = azurerm_resource_group.function_rg.location
+  resource_group_name = azurerm_resource_group.function_rg.name
+
+
+  service_plan_id            = azurerm_service_plan.function_plan.id
+  storage_account_name       = azurerm_storage_account.function_storage.name
+  storage_account_access_key = azurerm_storage_account.function_storage.primary_access_key
+  site_config {
+    # Node.js version is managed by FUNCTIONS_EXTENSION_VERSION and FUNCTIONS_WORKER_RUNTIME
+  }
+  app_settings = {
+    "FUNCTIONS_EXTENSION_VERSION" = "~4"
+    "FUNCTIONS_WORKER_RUNTIME"    = "node"
+    //"WEBSITE_NODE_DEFAULT_VERSION" = "20" # Specify the Node.js version
+    "AzureWebJobsStorage"         = azurerm_storage_account.function_storage.primary_connection_string
+  }
 }
